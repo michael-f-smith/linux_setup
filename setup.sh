1e68db31901b9865d5095f82b6bfa484cfd7ba88
Michael <mfs1036@rit.edu>
2025-02-21 17:06:45 -0500

    Update setup script and rename bash_aliases.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+
 # Add bash_aliases location to bashrc
-FILE="~/.bashrc"
-if [ -f "$FILE" ]
-then
-    echo "if [ -f ~/linux_setup/.bash_aliases ]; then" >> "$FILE"
-    echo "    .  ~/linux_setup/.bash_aliases" >> "$FILE"
-    echo "fi" >> "$FILE"
-fi
+#FILE="~/.bashrc"
+#if [ -f "$FILE" ]
+#then
+#    echo "if [ -f ~/linux_setup/.bash_aliases ]; then" >> "$FILE"
+#    echo "    .  ~/linux_setup/.bash_aliases" >> "$FILE"
+#    echo "fi" >> "$FILE"
+#fi
+
+
+# Create symbolic link to bash_aliases
+current_dir=$(pwd)
+aliases_file="$current_dir/bash_aliases"
+link_file="$HOME/.bash_aliases"
+echo "$aliases_file"
+echo "$link_file"
+ln -s "$aliases_file" "$link_file"
